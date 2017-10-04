bb07c9f0365dadbee4ac0fd19089a2af8ecae11c
Kevin Trinh <ktrinh1@nd.edu>
2017-10-03 22:55:24 -0400

    tabs

--- scibot.py
+++ scibot.py
@@ -12,54 +12,85 @@ import os
 
 if __name__ == '__main__':
 
-		textFolder		= 'data/text/'
-		microsoftFolder = 'data/microsoft/'
+	textFolder		= 'data/text/'
+	microsoftFolder = 'data/microsoft/'
 
-		# Open Index
+	# Open Index
 
-		indexFile = open(microsoftFolder + 'index.txt', 'r')
+	indexFile = open(microsoftFolder + 'index.txt', 'r')
 
-		index = {}
+	index = {}
 
-		for line in indexFile:
-			
-			l = line.split('\t')
+	for line in indexFile:
+		
+		l = line.split('\t')
 
-			folderName 	= l[0]
-			filename	= l[1]
-			pid			= l[2]
-			title		= l[3]
+		folderName 	= l[0]
+		filename	= l[1]
+		pid			= l[2]
+		title		= l[3]
 
-			i  = {}
-			i['folderName']	= folderName
-			i['filename']	= filename
-			i['title']		= title
+		i  = {}
+		i['folderName']	= folderName
+		i['filename']	= filename
+		i['title']		= title
 
-			index[pid]		= i
+		index[pid]		= i
 
-		indexFle.close()
+	indexFile.close()
 
-		papersFile = open(microsoftFolder + 'Papers.txt', 'r')
+	# Open Papers File
 
-		papers = {}
+	papersFile = open(microsoftFolder + 'Papers.txt', 'r')
 
-		for line in papersFile:
+	papers = {}
 
-			l = line.split('\t')
+	for line in papersFile:
 
-			pid 		= l[0]
-			title_case	= l[1]
-			title 		= l[2]
-			year		= l[3]
-			conf		= l[7]
-			cid			= l[9]
+		l = line.split('\t')
 
-			paper = {}
-			paper['title_case']
-			paper['title']
-			paper['year']
-			paper['conf']
-			paper['cid']
+		pid 		= l[0]
+		title_case	= l[1]
+		title 		= l[2]
+		year		= l[3]
+		conf		= l[7]
+		cid			= l[9]
+
+		paper = {}
+		paper['title_case'] = title_case
+		paper['title']		= title
+		paper['year']		= year
+		paper['conf']		= conf
+		paper['cid']		= cid
+
+		papers[pid]			= paper
+
+	papersFile.close()
+
+	# Open Paper Keywords File
+
+	paperKeywordsFile = open(microsoftFolder + 'PaperKeywords.txt', 'r')
+
+	paperKeywords = {}
+
+	for line in paperKeywords:
+
+		l = line.split('\t')
+
+		pid 		= l[0]
+		keyword 	= l[1]
+
+		if pid not in paperKeywords.keys():
+			paperKeywords[pid] = []
+		else:
+			paperKeywords[pid].append(keyword)
+
+	for key, value in paperKeywords:
+
+		print(key)
+		print(value)
+
+
 
 
 
