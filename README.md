746b0cf2149acaacf6fbf990141d5ddc8aa08343
Kevin Trinh <ktrinh1@nd.edu>
2017-10-03 22:45:39 -0400

    starting scibot

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # scibot
 Data Science Research Bot
+
+Group Members
+==============
+
+Brian Byrne (bbyrne5)
+
+Kevin Trinh (ktrinh1)
---
+++ scibot.py
@@ -0,0 +1,70 @@
+'''
+
+Data Science Research Bot (a.k.a SciBot)
+
+Brian Byrne (bbyrne5)
+Kevin Trinh (ktrinh1)
+
+'''
+
+import sys
+import os
+
+if __name__ == '__main__':
+
+		textFolder		= 'data/text/'
+		microsoftFolder = 'data/microsoft/'
+
+		# Open Index
+
+		indexFile = open(microsoftFolder + 'index.txt', 'r')
+
+		index = {}
+
+		for line in indexFile:
+			
+			l = line.split('\t')
+
+			folderName 	= l[0]
+			filename	= l[1]
+			pid			= l[2]
+			title		= l[3]
+
+			i  = {}
+			i['folderName']	= folderName
+			i['filename']	= filename
+			i['title']		= title
+
+			index[pid]		= i
+
+		indexFle.close()
+
+		papersFile = open(microsoftFolder + 'Papers.txt', 'r')
+
+		papers = {}
+
+		for line in papersFile:
+
+			l = line.split('\t')
+
+			pid 		= l[0]
+			title_case	= l[1]
+			title 		= l[2]
+			year		= l[3]
+			conf		= l[7]
+			cid			= l[9]
+
+			paper = {}
+			paper['title_case']
+			paper['title']
+			paper['year']
+			paper['conf']
+			paper['cid']
+
+
+
+
+
+
+
+
